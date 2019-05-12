41ea9e8a214cac0d010345722398a2d4f9b46381
Anton <apanchenko@gmail.com>
2019-05-12 21:14:45 +0300

    separate from game code

---
+++ src/core/bld.lua
@@ -0,0 +1,7 @@
+-- build configurations
+return 
+{
+  debug   = { id = 1, name = 'debug'},
+  develop = { id = 2, name = 'develop'},
+  release = { id = 3, name = 'release' },
+}
---
+++ src/core/package.lua
@@ -0,0 +1,5 @@
+local pkg = require 'src.core.pkg'
+
+return pkg
+  :new('src.core')
+  :load('arr', 'ass', 'cnt', 'env', 'evt', 'lay', 'log', 'map', 'obj', 'typ', 'vec', 'wrp')
