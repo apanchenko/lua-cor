01afe01fd3b00135804b6d7a9f7ec42f41488e85
Anton <apanchenko@gmail.com>
2019-05-13 12:02:13 +0300

    add comments

--- vec.lua
+++ vec.lua
@@ -1,26 +1,30 @@
+--[[
+    Vector or two elements. Extremely useful for 2D math.
+]]--
+
 local obj   = require 'src.luacor.obj'
 local typ   = require 'src.luacor.typ'
 local ass   = require 'src.luacor.ass'
 local wrp   = require 'src.luacor.wrp'
-local log   = require 'src.luacor.log'
 
--- 2d vector
+-- Define 2d vector type
 local vec = obj:extend('vec')
 vec.x = 0
 vec.y = 0
 
--- static ---------------------------------------------------------------------
---
+-- Static copy vector value from one table to another
 function vec.copy(from, to)
   to.x = from.x
   to.y = from.y
 end
---
+
+-- Static initialize table to screen center
 function vec.center(obj)
   obj.x = display.contentWidth / 2
   obj.y = display.contentHeight / 2
 end
---
+
+-- Math operators
 function vec.__add(l, r) return vec:new(l.x + r.x, l.y + r.y) end
 function vec.__sub(l, r) return vec:new(l.x - r.x, l.y - r.y) end
 function vec.__div(l, r) return vec:new(l.x / r.x, l.y / r.y) end
@@ -29,49 +33,54 @@ function vec.__eq (l, r) return (l.x == r.x) and (l.y == r.y) end
 function vec.__lt (l, r) return (l.x < r.x) and (l.y < r.y) end
 function vec.__le (l, r) return (l.x <= r.x) and (l.y <= r.y) end
 
--- create ---------------------------------------------------------------------
--- x, y
+-- Create a new vector
 function vec:new(x, y)
   return obj.new(self, {x = x, y = y})
 end
--- random
+
+-- Create a random vector in range
 function vec:random(min, max)
   return setmetatable({x = math.random(min.x, max.x), y = math.random(min.y, max.y)}, vec)
 end
---
+
+-- Create vector as a copy
 function vec:from(obj)
   return vec(obj.x, obj.y)
 end
 
--- methods ---------------------------------------------------------------------
---
+-- Convert to log friendly string
 function vec:__tostring()
   return '{'..self.x.. ",".. self.y..'}'
 end
--- square length
+
+-- Compute square length
 function vec:length2()
   return (self.x * self.x) + (self.y * self.y)
 end
--- round x,y to closest integer values
+
+-- Create new vector rounding x,y to closest integer values
 function vec:round()
   return vec:new(math.floor(self.x + 0.5), math.floor(self.y + 0.5))
 end
--- copy x,y into obj
+
+-- Copy x,y into obj
 function vec:to(obj)
   obj.x = self.x
   obj.y = self.y
 end
--- turn positive
+
+-- Create positive vector
 function vec:abs()
   return vec:new(math.abs(self.x), math.abs(self.y))
 end
 
--- constants ------------------------------------------------------------------
+-- Constant zero vector
 vec.zero = vec(0, 0)
+
+-- Constant ones vector
 vec.one = vec(1, 1)
 
--- module ---------------------------------------------------------------------
--- wrap vec functions
+-- Wrap vector functions
 function vec:wrap()
   wrp.wrap_stc_inf(vec, 'copy',   {'from', typ.tab}, {'to', typ.tab})
   wrp.wrap_stc_inf(vec, 'center', {'obj', typ.tab})
@@ -86,7 +95,7 @@ function vec:wrap()
   wrp.wrap_sub_inf(vec, 'abs')
 end
 
--- selftest
+-- Self test
 function vec:test()
   assert(tostring(vec.one) == '{1,1}')
   
@@ -103,5 +112,4 @@ function vec:test()
   ass(d:abs().x == 1.5)
 end
 
--- return module
 return vec
